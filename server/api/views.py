1de557f81bacd1138b6bf85200183c78244628af
christineJadeAvila <christinejadeavila05@gmail.com>
2025-02-13 23:49:28 +0800

    connecting f and b

---
+++ server/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from app1.models import Corder
+from .serializers import CorderSerializer
+from rest_framework import status
+
+@api_view(['GET'])
+def getData(request):
+    corders = Corder.objects.all()
+    serializer = CorderSerializer(corders, many=True)
+    return Response({'msg': 'Order Transactions', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def addData(request):
+    serializer = CorderSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
