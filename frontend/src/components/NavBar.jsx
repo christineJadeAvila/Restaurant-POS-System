83497a1c1154a2880aac4336e70b6060c668d28d
christineJadeAvila <christinejadeavila05@gmail.com>
2025-03-05 21:42:59 +0800

    added a live-clock

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,43 @@
 import "../styles/NavBar.css"
 import burgerMenu from "../assets/OrderMS/burgerMenu.svg"
+import calendar from "../assets/OrderMS/calendar.png"
+import clock from "../assets/OrderMS/clock.png"
+import React from "react"
+
 
 import DateObject from "react-date-object"
 
 function NavBar(props) {
-    
-    var date = new DateObject()
+
+    // live clock
+
+    const [time, setTime ] = React.useState(new Date());
+
+    React.useEffect(() => {
+        setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+    }, [])
+
+    let date = new DateObject()
 
     return(
         <>
             <section className="navigation--bar">
 
-                <img src={burgerMenu} alt="" />
+                <img className="burger--menu" src={burgerMenu} alt="" />
+
+                <img className="date--time" src={calendar} alt="" />
                 <p className="nav--date">{date.format("dddd, DD MMMM YYYY")}</p>
-                <p className="nav--time">{date.format(" hh:mm A")}</p>
+
+        
+                <img className="date--time" src={clock} alt="" />
+                <p>{time.toLocaleString("en-US", {
+
+                        timeStyle: "medium",
+                        hour12: true,
+
+                })} </p>
 
             </section>
         </>
